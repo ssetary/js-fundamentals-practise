c9c5468ba2a9f70ecc74f7ce945af0413d8dff91
ssetary <set.vusatiuk@gmail.com>
2022-08-14 22:45:12 +0000

    Added new file and value

---
+++ HOMEWORK/hw4.js
@@ -0,0 +1,57 @@
+// task1
+// function calcRectangleArea(width, height) {
+//     if (isNaN(width) || isNaN(height)) {
+//         throw new Error("invalid value");
+//     } else {
+//     return width * height;
+//   };
+// };
+  
+//   let s = calcRectangleArea(width, height);
+//   console.log(s);
+
+
+
+//   try {
+//     console.log(calcRectangleArea("b" * number));
+// } catch {
+//     console.log(exception);
+// };
+
+
+// task2
+// let checkAge = function (age) {
+//     let age = prompt("Please enter your age"); 
+//     if (age =  ) {
+//         console.log("The field is empty! Please enter your age");
+//     } else if (age == "b") {
+//         console.log("You entered a letter. Enter the number of your age again");
+//     } else if (age < 14);
+//         console.log("Access denied. you are underage"); 
+//     } else {
+//         return age;
+//     }
+
+//     try {
+//         let result = checkAge();
+//         console.log(result);
+//     } catch (exception) {
+//         console.log(exception.empty);
+//         console.log(exception.letter);
+//         console.log(exception.underage);
+//     }
+
+
+// task3
+// class MonthException {
+//     message = "Count the months of the year";
+//     name = "MonthException";
+// }
+// console.log(MonthException);
+
+let d = new Date();
+let n = d.getMonth();
+
+console.log(d.getMonth());
+
+// набрать в гугле метод месяцы getmonth() js примеры использования
